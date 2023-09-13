4f2c82a620860d4ba1970ad69ce139e607f2946f
Calvin Danyalson <calvindany1102@gmail.com>
2023-09-13 17:29:01 +0700

    Adding Login Page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./pages/App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
+import "./assets/css/Root.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,84 @@
+import { useNavigate } from "react-router-dom";
+
 import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+import "../assets/css/TextField.css";
+import "../assets/css/Login.css";
+import "../assets/css/Root.css";
 
 export default function Login() {
+  const navigate = useNavigate();
   return (
     <>
-      <TextField
-        required
-        id="outlined-required"
-        label="Required"
-        defaultValue="Username"
-      />
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "40px",
+          }}
+        >
+          <div className="form-typography text-primary-color">
+            <Typography variant="h4">Welcome Back! Cheff</Typography>
+            <Typography variant="p" className="normal-text">
+              Please login first
+            </Typography>
+          </div>
+          <div className="group-text-field text-primary-color">
+            <TextField
+              required
+              id="outlined-required"
+              label="Username"
+              defaultValue=""
+            />
+            <TextField
+              required
+              id="outlined-required"
+              label="Password"
+              defaultValue=""
+            />
+          </div>
+          <div className="form-typography text-primary-color">
+            <Typography variant="p" className="normal-text">
+              Forgot Password?&ensp;
+              <span
+                className="hyperlink"
+                onClick={() => navigate("/forgotpassword")}
+              >
+                Click Here
+              </span>
+            </Typography>
+          </div>
+          <div className="form-button">
+            <Button
+              variant="contained"
+              className="text-primary-color button-primary"
+            >
+              Login
+            </Button>
+          </div>
+          <div style={{}}>
+            <Typography variant="p" className="normal-text">
+              Dont have account?&ensp;
+              <span className="hyperlink" onClick={() => navigate("/signup")}>
+                Sign Up here
+              </span>
+            </Typography>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
