7f0642a7c1c0fe4a00983e808b4281cc374c5f99
Calvin Danyalson <calvindany1102@gmail.com>
2023-09-13 23:35:45 +0700

    Starting create Landing page

---
+++ src/pages/User/LandingPage.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+import BannerLanding1 from "@/assets/img/banner-landing.png";
+
+import "@/assets/css/Landing.css";
+
+export default function LandingPage() {
+  return <></>;
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,12 +5,14 @@ import Register from "../pages/Auth/Register";
 import ResetPassword from "../pages/Auth/ResetPassword";
 import NewPassword from "../pages/Auth/NewPassword";
 import EmailConfirmation from "../pages/Auth/EmailConfirmation";
+import LandingPage from "../pages/User/LandingPage";
 
 export default function Router() {
   const location = useLocation();
   return (
     <>
       <Routes location={location} key={location.pathname}>
+        <Route exact path="/" element={<LandingPage />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/resetpassword" element={<ResetPassword />} />
