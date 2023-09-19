5ae82c973ec77ecf72486312b6c7c5f3a864da6b
calvindany <calvindany1102@gmail.com>
2023-09-19 21:54:53 +0700

    Adding List Course component (Not Finished Yet)

---
+++ src/components/ListCourse.jsx
@@ -0,0 +1,57 @@
+import {
+  Container,
+  CardContent,
+  Card,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
+export default function ListCourse({ listImage }) {
+  return (
+    <>
+      <Container maxWidth="xl" style={{ marginBottom: "8vh" }}>
+        <div className="course-container">
+          <Typography
+            className="title-text text-primary-color"
+            style={{ marginBottom: "5vh" }}
+          >
+            More Professional Class
+          </Typography>
+          <div className="list-course-container">
+            {listImage.map((course, index) => (
+              <Card sx={{ minWidth: 400 }} key={index}>
+                <CardMedia
+                  sx={{ height: 250 }}
+                  image={course.image}
+                  title={course.title}
+                />
+                <CardContent sx={{ textAlign: "left" }}>
+                  <Typography variant="body2" color="text.secondary">
+                    {course.category}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    sx={{ marginBottom: "40px" }}
+                  >
+                    {course.title}
+                  </Typography>
+                  <Typography variant="h5" className="text-secondary-color">
+                    {course.price}
+                  </Typography>
+                </CardContent>
+              </Card>
+              // <CourseCard
+              //   image={course.image}
+              //   title={course.title}
+              //   category={course.category}
+              //   price={course.price}
+              // />
+            ))}
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
--- src/pages/User/LandingPage.jsx
+++ src/pages/User/LandingPage.jsx
@@ -14,6 +14,7 @@ import BannerLanding2 from "@/assets/img/banner-landing2.png";
 
 import BannerLanding from "@/components/Banner";
 import CategoryCard from "@/components/CategoryCard";
+import ListCourse from "../../components/ListCourse";
 import Footer from "@/components/Footer";
 
 import "@/assets/css/Landing.css";
@@ -168,49 +169,7 @@ export default function LandingPage() {
           </Card>
         </div>
       </Container>
-      <Container maxWidth="xl" style={{ marginBottom: "8vh" }}>
-        <div className="course-container">
-          <Typography
-            className="title-text text-primary-color"
-            style={{ marginBottom: "5vh" }}
-          >
-            More Professional Class
-          </Typography>
-          <div className="list-course-container">
-            {data.map((course, index) => (
-              <Card sx={{ minWidth: 400 }} key={index}>
-                <CardMedia
-                  sx={{ height: 250 }}
-                  image={course.image}
-                  title={course.title}
-                />
-                <CardContent sx={{ textAlign: "left" }}>
-                  <Typography variant="body2" color="text.secondary">
-                    {course.category}
-                  </Typography>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="div"
-                    sx={{ marginBottom: "40px" }}
-                  >
-                    {course.title}
-                  </Typography>
-                  <Typography variant="h5" className="text-secondary-color">
-                    {course.price}
-                  </Typography>
-                </CardContent>
-              </Card>
-              // <CourseCard
-              //   image={course.image}
-              //   title={course.title}
-              //   category={course.category}
-              //   price={course.price}
-              // />
-            ))}
-          </div>
-        </div>
-      </Container>
+      <ListCourse listImage={data} />
       <BannerLanding
         image={BannerLanding2}
         titleSize="big"
